1bbf51321fba6638007d76ef59f0c0bfc142ddf7
El Hadi Moussi <50993404+ehmoussi@users.noreply.github.com>
2025-03-16 11:05:47 +0100

    Add ssl wrapper

--- client.py
+++ client.py
@@ -1,15 +1,19 @@
 import socket
+import ssl
 
 HOSTNAME = "0.0.0.0"
 PORT = 3000
 
 
 def main() -> None:
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.setblocking(False)
+    context = ssl.create_default_context()
+    context.load_verify_locations(cafile="cert.pem")
+    context.check_hostname = False
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_sock:
+        client_sock.setblocking(False)
         while True:
             try:
-                s.connect((HOSTNAME, PORT))
+                client_sock.connect((HOSTNAME, PORT))
             except BlockingIOError:
                 pass
             except Exception:
@@ -17,34 +21,45 @@ def main() -> None:
             else:
                 is_connected = True
                 break
-        print("Echo server")
-        while is_connected:
-            client_data = input()
-            s.sendall(client_data.encode())
-            server_data: bytes | None = None
+        with context.wrap_socket(
+            client_sock, do_handshake_on_connect=False, server_hostname=HOSTNAME
+        ) as ssl_client_sock:
+            ssl_client_sock.do_handshake(block=True)
             while True:
                 try:
-                    recv_data = s.recv(1024)
-                except BlockingIOError:
-                    if server_data is None:
-                        continue
-                    else:
-                        break
-                except Exception:
-                    server_data = None
-                    is_connected = False
-                    break
+                    ssl_client_sock.do_handshake(block=False)
+                except ssl.SSLWantReadError:
+                    pass
                 else:
-                    if recv_data == b"":
+                    break
+            print("Echo server")
+            while is_connected:
+                client_data = input()
+                ssl_client_sock.sendall(client_data.encode())
+                server_data: bytes | None = None
+                while True:
+                    try:
+                        recv_data = ssl_client_sock.recv(1024)
+                    except ssl.SSLWantReadError:
+                        if server_data is None:
+                            continue
+                        else:
+                            break
+                    except Exception:
                         server_data = None
                         is_connected = False
                         break
                     else:
-                        if server_data is None:
-                            server_data = b""
-                        server_data += recv_data
-            if server_data is not None:
-                print(f"Received from the server: {server_data.decode()}")
+                        if recv_data == b"":
+                            server_data = None
+                            is_connected = False
+                            break
+                        else:
+                            if server_data is None:
+                                server_data = b""
+                            server_data += recv_data
+                if server_data is not None:
+                    print(f"Received from the server: {server_data.decode()}")
 
 
 if __name__ == "__main__":
--- server.py
+++ server.py
@@ -1,13 +1,14 @@
 import socket
 import logging
 import atexit
+import ssl
 
 HOSTNAME = "0.0.0.0"
 PORT = 3000
-POOL_SOCKETS: dict[tuple[str, str], socket.socket] = {}
+POOL_SOCKETS: dict[tuple[str, str], ssl.SSLSocket] = {}
 MAX_CLIENTS = 10
 
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger("")
 
 
@@ -22,61 +23,71 @@ def clean_pool():
 
 def main() -> None:
     global POOL_SOCKETS
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.setblocking(False)
-        s.bind((HOSTNAME, PORT))
-        s.listen(MAX_CLIENTS)
-        while True:
-            if len(POOL_SOCKETS) < MAX_CLIENTS:
-                try:
-                    sock, addr_info = s.accept()
-                except BlockingIOError:
-                    pass
+    context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
+    context.load_cert_chain(certfile="cert.pem", keyfile="key.pem")
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_sock:
+        server_sock.setblocking(False)
+        server_sock.bind((HOSTNAME, PORT))
+        server_sock.listen(MAX_CLIENTS)
+        with context.wrap_socket(server_sock, server_side=True) as ssl_server_sock:
+            while True:
+                if len(POOL_SOCKETS) < MAX_CLIENTS:
+                    try:
+                        client_sock, addr_info = ssl_server_sock.accept()
+                    except BlockingIOError:
+                        pass
+                    except Exception:
+                        logger.info("A client failed to connect")
+                    else:
+                        POOL_SOCKETS[addr_info] = client_sock
+                        client_sock.setblocking(False)
+                        logger.info(
+                            "%s just connected (blocking: %s)",
+                            addr_info,
+                            client_sock.getblocking(),
+                        )
                 else:
-                    POOL_SOCKETS[addr_info] = sock
-                    sock.setblocking(False)
-                    logger.info(
-                        "%s just connected (blocking: %s)",
-                        addr_info,
-                        sock.getblocking(),
+                    logger.warning(
+                        "Can't accept the connection. The maximum number of clients (%s) have been reached"
                     )
-            else:
-                logger.warning(
-                    "Can't accept the connection. The maximum number of clients (%s) have been reached"
-                )
-            for addr_info, sock in list(POOL_SOCKETS.items()):
-                data: bytes | None = None
-                try:
-                    while True:
-                        try:
-                            recv_data = sock.recv(1024)
-                        except BlockingIOError:
-                            break
-                        else:
-                            if recv_data == b"":
-                                del POOL_SOCKETS[addr_info]
-                                logger.info("%s disconnect from the server", addr_info)
+                for addr_info, client_sock in list(POOL_SOCKETS.items()):
+                    data: bytes | None = None
+                    try:
+                        while True:
+                            try:
+                                recv_data = client_sock.recv(1024)
+                            except ssl.SSLWantReadError:
                                 break
                             else:
-                                if data is None:
-                                    data = b""
-                                data += recv_data
-                except Exception:
-                    logger.info(
-                        "%s has disconnect from the server for an unknown reason",
-                        addr_info,
-                    )
-                    del POOL_SOCKETS[addr_info]
-                else:
-                    if data is None:
-                        continue
-                    elif data == b"":
+                                if recv_data == b"":
+                                    del POOL_SOCKETS[addr_info]
+                                    logger.info(
+                                        "%s disconnect from the server", addr_info
+                                    )
+                                    break
+                                else:
+                                    if data is None:
+                                        data = b""
+                                    data += recv_data
+                    except Exception as e:
+                        logger.info(
+                            "%s has disconnect from the server for an unknown reason",
+                            addr_info,
+                        )
+                        logger.debug("%s", e)
                         del POOL_SOCKETS[addr_info]
-                        logger.info("%s disconnect from the server", addr_info)
                     else:
-                        logger.info("Received from %s: %s", addr_info, data.decode())
-                        sock.sendall(data)
-                        logger.info("Send to %s: %s", addr_info, data.decode())
+                        if data is None:
+                            continue
+                        elif data == b"":
+                            del POOL_SOCKETS[addr_info]
+                            logger.info("%s disconnect from the server", addr_info)
+                        else:
+                            logger.info(
+                                "Received from %s: %s", addr_info, data.decode()
+                            )
+                            client_sock.sendall(data)
+                            logger.info("Send to %s: %s", addr_info, data.decode())
 
 
 if __name__ == "__main__":
